d22e8044310e00c0717cddad4d5dee6c8e75e23f
Mariano Genin <genin_mariano@hotmail.com>
2023-10-04 22:10:59 -0300

    modifico preentregable

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const datosPersonales =[]
 
 function cargaDatos (){
+    for(let i =0; i<3; i++){
     let nombreApellidos = prompt(`por favor ingrese su nombre y apellido`)
 
     while( nombreApellidos.trim() === ""){
@@ -9,22 +10,18 @@ function cargaDatos (){
 
 }
 if(datosPersonales.includes(nombreApellidos)){
-    alert(`el nombre de usuario ${nombreApellidos} ya esta registrado por favor ingrese otro`)
+    confirm(`el nombre de usuario ${nombreApellidos} ya esta registrado por favor ingrese otro`)
 
-}else{
+}else {
+    (datosPersonales.push(nombreApellidos))
     alert(`binvenido ${nombreApellidos} `)
 }
-if(datosPersonales!=nombreApellidos){
-    datosPersonales.push(nombreApellidos)
-}else{
-    cargaDatos()
+
+ 
 }
 
 }
 
 
-for( let i=0; i<=10;i++ ){
-    cargaDatos()
-}
 console.log(datosPersonales)
 cargaDatos()
